d8e9aff9e21afc9fd73c8ae9ab25437e91357e3e
Dileep <anjanidileepkoppu@gmail.com>
2023-03-16 02:41:50 +0000

    first commit

---
+++ my-notes-app/server/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const connectToDatabase = require("./database/dbConfig");
+const noteRoutes = require("./routes/index");
+const errorMiddleware = require("./middlewares/error");
+const helmet = require('helmet')
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(helmet())
+
+app.use(noteRoutes);
+
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: false,
+    errors: [
+      {
+        msg: "Route not found",
+      },
+    ],
+  });
+});
+
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3001;
+connectToDatabase().then((_) => {
+  app.listen(PORT, (_) => {
+    console.log(`Server started on port ${PORT}`);
+    console.log(app.get('env')); 
+  });
+});
